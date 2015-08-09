2d2e2bd810ad97e6389ddea6735248e0db1789bc
gigaherz <gigaherz@gmail.com>
2015-08-09 23:56:32 +0200

    Update icon. Update version in build.gradle. Make the mod version take the version number from the build.gradle. Mod is now ready for its first release. :)

--- src/main/java/gigaherz/enderRift/EnderRiftMod.java
+++ src/main/java/gigaherz/enderRift/EnderRiftMod.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = EnderRiftMod.MODID, version = EnderRiftMod.VERSION, dependencies = "after:Waila;after:NotEnoughItems")
 public class EnderRiftMod {
     public static final String MODID = "enderRift";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@VERSION";
 
     public static final String CHANNEL = "EnderRift";
 
