81f51d5601a71677749ec730d35b2b0eaef3d64e
gigaherz <gigaherz@gmail.com>
2016-04-24 22:52:22 +0200

    Added logic to first try to fill existing inventories with the blocks they already contain, rather than always using the closest one.

--- src/main/java/gigaherz/enderRift/ConfigValues.java
+++ src/main/java/gigaherz/enderRift/ConfigValues.java
@@ -1,6 +1,7 @@
 package gigaherz.enderRift;
 
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 
 public class ConfigValues
 {
@@ -13,9 +14,16 @@ public class ConfigValues
     public static float PowerPerExtractionLinear;
     public static float PowerPerExtractionGeometric;
 
+    public static boolean PreferContainersWithExistingStacks;
+
     public static void readConfig(Configuration config)
     {
         config.load();
+
+        Property cfg = config.get("General", "PreferContainersWithExistingStacks", true);
+        cfg.comment = "If the game lags when using the Rift Browser or Interface, disable this to make item insertion take a bit less time.";
+        PreferContainersWithExistingStacks = cfg.getBoolean();
+
 
         PowerPerInsertionConstant = (float) config.get("PowerUsage", "PowerPerInsertionConstant", 1.23).getDouble();
         PowerPerInsertionLinear = (float) config.get("PowerUsage", "PowerPerInsertionLinear", 0.93).getDouble();
